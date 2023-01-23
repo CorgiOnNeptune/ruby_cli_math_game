28f2daf4679a4564ea2a5662b9552ac3f3f98329
Justin <justinjdbennett@gmail.com>
2023-01-23 16:30:48 -0700

    Setup GameState class

---
+++ GameState.rb
@@ -0,0 +1,8 @@
+require './Player.rb'
+require './Questions.rb'
+
+class GameState
+  def initialize
+    @turn = 1
+  end
+end
---
+++ main.rb
@@ -0,0 +1,6 @@
+require './GameState'
+require './Player'
+require './Questions'
+
+player1 = Player.new('Andy')
+player2 = Player.new('Chucky')
