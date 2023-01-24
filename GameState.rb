f295de0e8b92fce40a8237afe741887e83138c99
Justin <justinjdbennett@gmail.com>
2023-01-23 18:57:34 -0700

    Implement GameState class and all game logic.

--- GameState.rb
+++ GameState.rb
@@ -1,8 +1,44 @@
-require './Player.rb'
-require './Questions.rb'
-
 class GameState
-  def initialize
+  attr_accessor :turn, :ended
+
+  def initialize(player1, player2)
     @turn = 1
+    @player1 = player1
+    @player2 = player2
+    @ended = false
+  end
+
+  def start_game
+    puts "\n----- START GAME -----"
+    puts "#{@player1.name} vs #{@player2.name}"
+  end
+
+  def current_player
+    if @turn == 1
+      @player1
+    else
+      @player2
+    end
+  end
+
+  def next_turn
+    puts "\n----- NEW TURN -----"
+    @turn = (@turn == 1 ? 2 : 1)
+  end
+
+  def determine_winner
+    if @player1.lives.zero?
+      puts "#{@player2.name} is the winner with #{@player2.lives}/3 lives remaining!"
+    elsif @player2.lives.zero?
+      puts "#{@player1.name} is the winner with #{@player1.lives}/3 lives remaining!"
+    end
+  end
+
+  def end_game
+    puts "\n----- GAME OVER -----"
+    determine_winner
+
+    puts 'GG!'
+    @ended = true
   end
 end
--- main.rb
+++ main.rb
@@ -1,6 +1,32 @@
 require './GameState'
 require './Player'
-require './Questions'
+require './Question'
 
-player1 = Player.new('Andy')
-player2 = Player.new('Chucky')
+# player1 = Player.new('Andy')
+# player2 = Player.new('Chucky')
+
+puts 'Player 1, what is your name?'
+player1 = Player.new
+
+puts 'Player 2, what is your name?'
+player2 = Player.new
+
+game = GameState.new(player1, player2)
+game.start_game
+
+until game.ended
+  question = Question.new
+  question.ask_question(game.current_player.name)
+
+  game.current_player.lose_life unless question.check_answer(gets.chomp)
+
+  puts "#{player1.name}: #{player1.lives}/3 vs #{player2.name}: #{player2.lives}/3"
+
+  if game.current_player.lives.zero?
+    game.end_game
+    player1 = nil
+    player2 = nil
+  else
+    game.next_turn
+  end
+end
